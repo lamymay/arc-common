19c1eaa6618f1e78609ad5c76340697f7fabce8d
lamymay <lamymay@outlook.com>
2019-07-10 17:56:06 +0800

    加入一些代码

--- entity/src/main/java/com/arc/model/domain/system/SysMenu.java
+++ entity/src/main/java/com/arc/model/domain/system/SysMenu.java
@@ -5,7 +5,8 @@ import com.arc.model.BaseModel;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -32,5 +33,33 @@ public class SysMenu extends BaseModel {
     private String note;// 备注
     private Long systemId;// 适用系统的id（备用）
     private Date createDate;// 创建时间
-    private LocalDateTime updateDate;// 更新时间
+    private Date updateDate;// 更新时间
+//    private LocalDateTime updateDate;// 更新时间
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        ArrayList<Long> list1 = new ArrayList<>();
+        ArrayList<Long> list2 = new ArrayList<>();
+        ArrayList<BigInteger> list3 = new ArrayList<>();
+
+        list1.add(1000L);
+        list1.add(1001L);
+        list1.add(1002L);
+        list1.add(1003L);
+        list2.add(1002L);
+
+        for (Long b : list2) {
+            System.out.println(list1.contains(b));
+            for (Long a : list1) {
+                System.out.println(a==b);
+                System.out.println(a.equals(b));
+                System.out.println(a);
+                System.out.println(b);
+            }
+
+        }
+
+
+    }
+
 }
