8718443b9894a4c94d6613ff6138dabd11f2646f
lamymay <lamymay@outlook.com>
2019-07-14 14:15:41 +0800

    修正mapper报错

---
+++ entity/src/main/java/com/arc/model/request/agile/ProjectRequest.java
@@ -0,0 +1,16 @@
+package com.arc.model.request.agile;
+
+import com.arc.model.request.Query;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author may
+ * @since 2019/7/13 22:39
+ */
+@Slf4j
+@Setter
+@Getter
+public class ProjectRequest extends Query {
+}
