6cbe55707ddb5ae0665dd5b1809c4471097a903f
lamymay <lamymay@outlook.com>
2019-07-21 21:14:03 +0800

    学习健康管理

---
+++ core/src/main/java/com/arc/annotation/Note.java
@@ -0,0 +1,90 @@
+package com.arc.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 备注： 表示一些说明
+ *
+ * @author may
+ * @since 2019/7/21 11:49
+ */
+//@Target 说明了Annotation所修饰的对象范围
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+//@Documented是说明注释用在类上的作用：给类添加注释
+@Documented
+public @interface Note {
+
+    /**
+     * 资源名
+     *
+     * @return
+     */
+    //@AliasFor("value")
+//    String name() default "";
+
+
+    //@AliasFor("name")
+    String value() default "";
+
+
+//    /**
+//     * 优先级
+//     *
+//     * @return
+//     */
+//    int priority();
+
+//    /**
+//     * 【资源类型 枚举】（PART，LINK，ACTION）
+//     *
+//     * @return
+//     */
+//    SysResourceType type();
+
+
+}
+
+//注解的元注解说明：
+//@Target：
+//
+//　　　@Target说明了Annotation所修饰的对象范围：Annotation可被用于 packages、types（类、接口、枚举、Annotation类型）、类型成员（方法、构造方法、成员变量、枚举值）、方法参数和本地变量（如循环变量、catch参数）。在Annotation类型的声明中使用了target可更加明晰其修饰的目标。
+//
+//　　作用：用于描述注解的使用范围（即：被描述的注解可以用在什么地方）
+//ElementType
+//public enum ElementType {
+//    /**用于描述类、接口(包括注解类型) 或enum声明 Class, interface (including annotation type), or enum declaration */
+//    TYPE,
+//
+//    /** 用于描述域 Field declaration (includes enum constants) */
+//    FIELD,
+//
+//    /**用于描述方法 Method declaration */
+//    METHOD,
+//
+//    /**用于描述参数 Formal parameter declaration */
+//    PARAMETER,
+//
+//    /**用于描述构造器 Constructor declaration */
+//    CONSTRUCTOR,
+//
+//    /**用于描述局部变量 Local variable declaration */
+//    LOCAL_VARIABLE,
+//
+//    /** Annotation type declaration */
+//    ANNOTATION_TYPE,
+//
+//    /**用于描述包 Package declaration */
+//    PACKAGE,
+//
+//    /**
+//     * 用来标注类型参数 Type parameter declaration
+//     * @since 1.8
+//     */
+//    TYPE_PARAMETER,
+//
+//    /**
+//     *能标注任何类型名称 Use of a type
+//     * @since 1.8
+//     */
+//    TYPE_USE
--- entity/src/main/java/com/arc/model/domain/system/SysResource.java
+++ entity/src/main/java/com/arc/model/domain/system/SysResource.java
@@ -16,14 +16,20 @@ public class SysResource extends BaseModel {
 
 	private static final long serialVersionUID = 1L;
 
-	private Long  id;// 主键
-	private Date createDate;// 创建时间
-	private String method;// 请求方法（GET，POST，ALL）
-	private String note;// 备注
-	private String parentId;// 上级节点ID
-	private String path;// 资源路径
-	private Integer priority;// 优先级
-	private String resourceName;// 资源名
-	private String type;// 资源类型（PART，LINK，ACTION）
-	private Date updateDate;// 修改时间
+    private Long id;// 主键
+
+    private String parentId;// 上级节点ID
+
+    private String resourceName;// 资源名
+
+    private String path;// 资源路径
+
+    private String method;// 请求方法（GET，POST，ALL）
+
+    private String note;// 备注
+    private Integer priority;// 优先级
+    private String type;// 资源类型（PART，LINK，ACTION）
+    private int state;
+    private Date createDate;// 创建时间
+    private Date updateDate;// 修改时间
 }
