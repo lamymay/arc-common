84eb85eead1f91e5701045bc75f602fc4a67cad6
lamy <lamymay@outlook.com>
2019-09-30 11:54:37 +0800

    数据字典

---
+++ core/src/main/java/com/arc/model/domain/system/RoleResource.java
@@ -0,0 +1,28 @@
+package com.arc.model.domain.system;
+
+import com.arc.model.BaseModel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author 叶超
+ * @since 2019/9/29 23:13
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleResource extends BaseModel {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long roleId;
+
+    private Long resourceId;
+
+    public RoleResource(Long id, Long resourceId) {
+        super();
+        this.roleId = roleId;
+        this.resourceId = resourceId;
+    }
+}
