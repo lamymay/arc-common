992d881f07394e8982b55a2dc5617f3eff6c9363
lamymay <lamymay@outlook.com>
2020-03-07 22:58:24 +0800

    改进文件 数据记录

--- core/src/main/java/com/arc/core/model/domain/system/SysFile.java
+++ core/src/main/java/com/arc/core/model/domain/system/SysFile.java
@@ -19,26 +19,39 @@ public class SysFile extends BaseModel {
 
     private Long id;//
     private String code;// 版本号
+
+    //文件 本身属性： 名称/大小/后缀/文件位置/类型
     private String name;// 显示名称
+    private Long length;// 文件大小
+    private String lengthUnit;// 文件大小单位
+
     private String suffix;// 后缀
     private String type;// 类型
+    private String note;// 描述
+
+    private String path;// 文件存放位置--如果是自己的环境应该是服务器的绝对路径
+
+    private String location;// 文件持久化位置，用于 判断拼接url前部分    SERVER / DEVELOP
+    private String host;// 文件持久化位置，用于 判断拼接url前部分    SERVER / DEVELOP  或者偷懒直接写 ip
+    private String uri;// 文件存放 url=      {host}:{port} +/xxx/yyy/zzz
+
+
     private Integer version;// 版本信息id
-
-    //todo  rename length
-    private Long size;// 文件大小
-    private String sizeUnit;// 文件大小单位
-    private String remark;// 描述
-
-    private String url;// 文件存放url
-    private String path;// 文件存放url
-
     private Integer state;// 逻辑删除用的标识
 
     private String key;// 文件标识
     private String checkType;// 校验方式
     private String checkCode;// 校验码
+
     private Date createTime;// 创建时间
     private Date updateTime;// 更新时间
 
 
 }
+
+//URI = Universal Resource Identifier 统一资源标志符，用来标识抽象或物理资源的一个紧凑字符串。
+//URL = Universal Resource Locator 统一资源定位符，一种定位资源的主要访问机制的字符串，一个标准的URL必须包括：protocol、host、port、path、parameter、anchor。
+//URN = Universal Resource Name 统一资源名称，通过特定命名空间中的唯一名称或ID来标识资源。
+//————————————————
+//版权声明：本文为CSDN博主「koflance@西溪」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+//原文链接：https://blog.csdn.net/koflance/article/details/79635240
