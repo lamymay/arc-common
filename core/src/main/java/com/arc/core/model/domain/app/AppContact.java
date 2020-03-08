74fcf2645b906ba25869a854e963ed8dae2a3586
lamymay <lamymay@outlook.com>
2020-03-08 14:31:23 +0800

    172.17.0.17

--- core/src/main/java/com/arc/core/model/domain/app/AppContact.java
+++ core/src/main/java/com/arc/core/model/domain/app/AppContact.java
@@ -20,6 +20,7 @@ public class AppContact {
     private static final long serialVersionUID = 1L;
 
     //本系统中的id是主键
+//    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     //是每个通讯录中的id，不唯一，每个设备上的通讯录id各自独立
