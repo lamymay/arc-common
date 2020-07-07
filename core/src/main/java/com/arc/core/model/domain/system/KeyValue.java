89df57763140e9f960626384f58ada34237e6937
yechao <wb-yc698649@alibaba-inc.com>
2020-07-07 19:08:26 +0800

    修正启动报错

--- core/src/main/java/com/arc/core/model/domain/system/KeyValue.java
+++ core/src/main/java/com/arc/core/model/domain/system/KeyValue.java
@@ -1,7 +1,6 @@
 package com.arc.core.model.domain.system;
 
 import com.arc.core.enums.common.EnableEnum;
-import com.arc.core.enums.common.SexEnum;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -20,33 +19,46 @@ public class KeyValue implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Long id;// 主键
+    /**
+     * 主键id
+     */
+    private Long id;
+
+    /**
+     * key
+     */
     private String key;
+
+    /**
+     * 值-字符串格式
+     */
     private String value;
 
-    private String type;
+//    /**
+//     * 值-二进制属性测试
+//     */
+//    private byte[] valueBinary;//列值 blob类型
+
+    /**
+     * 适用范围,即类型
+     */
+    private Integer range;
+
+    /**
+     * 注释
+     */
     private String note;
 
-    /**
-     * 枚举属性测试1
-     */
-    private EnableEnum enable;
+//    /**
+//     * 枚举属性测试,非零=true=可用的/零=false=不可用(默认)
+//     */
+//    private EnableEnum enable;
 
 
-    /**
-     * 枚举属性测试2
-     */
-    private SexEnum sex;
-
-    /**
-     * 二进制属性测试
-     */
-    private byte[] valueBinary;//列值 blob类型
-
-    public KeyValue(String key, String value, String type, String note) {
+    public KeyValue(String key, String value, Integer range, String note) {
         this.key = key;
         this.value = value;
-        this.type = type;
+        this.range = range;
         this.note = note;
 
     }
