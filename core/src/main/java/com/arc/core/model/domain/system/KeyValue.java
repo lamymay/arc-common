a32a8c08437573474b479a41a2cacc09ed832590
yechao <wb-yc698649@alibaba-inc.com>
2020-07-10 12:46:35 +0800

    加接口

--- core/src/main/java/com/arc/core/model/domain/system/KeyValue.java
+++ core/src/main/java/com/arc/core/model/domain/system/KeyValue.java
@@ -1,11 +1,11 @@
 package com.arc.core.model.domain.system;
 
-import com.arc.core.enums.common.EnableEnum;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * key-value表，偷懒用
@@ -19,40 +19,17 @@ public class KeyValue implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    /**
-     * 主键id
-     */
-    private Long id;
+    private Long id;//主键id
+    private String key;//key
+    private String value;//值-字符串格式
 
-    /**
-     * key
-     */
-    private String key;
+    //    private byte[] valueBinary;//值-二进制属性测试,blob类型
+    private Integer range;//适用范围,即类型
+    private String note;//注释
 
-    /**
-     * 值-字符串格式
-     */
-    private String value;
-
-//    /**
-//     * 值-二进制属性测试
-//     */
-//    private byte[] valueBinary;//列值 blob类型
-
-    /**
-     * 适用范围,即类型
-     */
-    private Integer range;
-
-    /**
-     * 注释
-     */
-    private String note;
-
-//    /**
-//     * 枚举属性测试,非零=true=可用的/零=false=不可用(默认)
-//     */
-//    private EnableEnum enable;
+    private LocalDateTime createTime;// 创建时间
+    private LocalDateTime updateTime;// 更新时间
+    //    private EnableEnum enable;//枚举属性测试,非零=true=可用的/零=false=不可用(默认)
 
 
     public KeyValue(String key, String value, Integer range, String note) {
